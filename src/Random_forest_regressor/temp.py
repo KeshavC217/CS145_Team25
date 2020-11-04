f662133ad4f21d937ca6f7b78a750b5116b918f4
Keshav Chakrapani <keshavc@gmail.com>
2020-11-03 23:12:31 -0800

    Changed max iter to 500

--- src/Random_forest_regressor/temp.py
+++ src/Random_forest_regressor/temp.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression, Perceptron
 import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, RobustScaler, MinMaxScaler
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 
@@ -18,13 +19,13 @@ def string_process(x):
 data = pd.read_csv("../../data/train.csv")
 data = data.fillna(data.mean())
 data = data[['Date', 'Confirmed']]
-data_alabama = data.iloc[46::50,:]
+data_alabama = data.iloc[4::50,:]
 X_temp = data_alabama.iloc[:,0:1].values
 X = np.array([[string_process(x)] for x in X_temp])
 y = data_alabama.iloc[:,1].values
 polynomial_features= PolynomialFeatures(degree=3)
 #Xpol = polynomial_features.fit_transform(X)
-reg = SVR(C=1e5, max_iter=1000)
+reg = SVR(C=1e5, max_iter=500)
 reg.fit(X,y)
 plt.plot(X,y)
 plt.plot(X,reg.predict(X))
